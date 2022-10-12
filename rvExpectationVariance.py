8677e3951d7e21a3fc3f73196de76df657de5e86
Varad Kulkarni <83868670+Hades62442@users.noreply.github.com>
2022-10-12 20:22:09 +0100

    refactor expectation variance file with snake_case

--- rvExpectationVariance.py
+++ rvExpectationVariance.py
@@ -3,40 +3,43 @@
 
 from math import sqrt
 
-def getInputs():
-    outcomeCount = int(input("Enter how many outcomes you have: "))
+def get_inputs():
+    outcome_count = int(input("Enter how many outcomes you have: "))
 
     outcomes, probabilities = [], []
 
-    for i in range(outcomeCount):
+    for i in range(outcome_count):
         outcomes.append(int(input(f"Enter value of outcome {i+1}: ")))
         probability_i = (input(f"Enter probability of outcome {i+1}: "))
         # convert probability to float
-        num, denom = probability_i.split('/')
-        probabilities.append(float(num)/float(denom))
+        if '/' in probability_i:
+            num, denom = probability_i.split('/')
+            probabilities.append(float(num)/float(denom))
+        else:
+            probabilities.append(float(probability_i))
 
     return outcomes, probabilities
 
 
-def getExpectedValue(outcomes, probabilities):
-    expectedVal = 0
+def get_expected_value(outcomes, probabilities):
+    expected_val = 0
     for i in range(len(outcomes)):
-        expectedVal += outcomes[i] * probabilities[i]
+        expected_val += outcomes[i] * probabilities[i]
 
-    return expectedVal
+    return expected_val
 
 
-def getVarianceValue(outcomes, probabilities, expectedVal):
-    varianceVal = 0
+def get_variance_value(outcomes, probabilities, expected_val):
+    variance_val = 0
     for i in range(len(outcomes)):
-        varianceVal += (outcomes[i]-expectedVal)**2 * probabilities[i]
+        variance_val += (outcomes[i]-expected_val)**2 * probabilities[i]
 
-    return varianceVal
+    return variance_val
 
 
-out, prob = getInputs()
-exp = getExpectedValue(out, prob)
-var = getVarianceValue(out, prob, exp)
+out, prob = get_inputs()
+exp = get_expected_value(out, prob)
+var = get_variance_value(out, prob, exp)
 std = sqrt(var)
 
 print(f"Expected value: {exp}\nVariance value: {var}\nStandard deviation: {std}")
