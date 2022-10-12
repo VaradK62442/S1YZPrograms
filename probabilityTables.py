d8c4766224a300d092884693c5855cfe670c9b3c
Varad Kulkarni <83868670+Hades62442@users.noreply.github.com>
2022-10-12 12:46:34 +0100

    make table and set titles in probabilityTables

---
+++ probabilityTables.py
@@ -0,0 +1,42 @@
+# form a table with n rows and m cols
+# calculate given probability
+# incl using bayes theorem
+
+def create_empty_table():
+    rows = int(input("Enter number of rows: "))
+    cols = int(input("Enter number of cols: "))
+    # include row and col for titles
+    # also include total col and row
+    table = []
+    for i in range(rows+2): # +2 accounting for title and total row
+        temp = []
+        for j in range(cols+2):# +2 accounting for title and total cols
+            # if title row:
+            if i == 0 or j == 0:
+                temp.append("x")
+            # if total row:
+            elif i == rows+2 or j == cols + 2:
+                temp.append("t")
+            # otherwise:
+            else:
+                temp.append(0)
+        table.append(temp)
+
+    return table
+
+def set_titles(table):
+    for i in range(1, len(table[0])-1): # -1 accounting for total col and row
+        table[0][i] = input(f"Enter title for column {i}: ")
+
+    for j in range(1, len(table)-1):
+        table[j][0] = input(f"Enter title for row {j}: ")
+
+    table[0][0] = input("Enter title for entire table: ")
+
+    return table
+
+table = create_empty_table()
+table = set_titles(table)
+
+for i in table:
+    print(i)
