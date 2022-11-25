41590716bcc1e41f0eb6af0bcb20c90f1d186b22
Varad Kulkarni <83868670+Hades62442@users.noreply.github.com>
2022-11-25 17:13:42 +0000

    if name == main

---
+++ basicCalc.py
@@ -0,0 +1,33 @@
+# find sum
+# find mean
+# find variance
+# find quartiles
+# find upper and lower fence
+# find outliers
+
+from math import sqrt
+
+def get_list():
+    n = int(input("Enter number of elements in data: "))
+    return [int(input(f"Enter element {i+1}: ")) for i in range(n)]
+
+
+def main():
+    l = get_list()
+    total = sum(l)
+    n = len(l)
+    mean = total / n
+    stdev = sum([(elt - mean) for elt in l]) / (n-1)
+    var = sqrt(stdev)
+
+    print(f'''
+    Inputted values: {l}\n
+    Sum: {total}\n
+    Mean: {mean}\n 
+    Standard deviation: {stdev}\n 
+    Variance: {var}\n 
+    ''')
+
+
+if __name__ == "__main__":
+    main()
--- chiSquareTestTables.py
+++ chiSquareTestTables.py
@@ -69,4 +69,5 @@ def main():
     print(f"Chi square t stat: {chisq}\nDegrees of freedom: {df}\np-value: {p_val}")
 
 
-main()
+if __name__ == "__main__":
+    main()
