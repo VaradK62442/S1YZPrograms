63cd71645e0c9025e12ff8bc92f8f257f85ebb75
Hades62442 <83868670+Hades62442@users.noreply.github.com>
2022-11-26 17:01:06 +0000

    basic calc complete

--- basicCalc.py
+++ basicCalc.py
@@ -6,26 +6,29 @@
 # find outliers
 
 from math import sqrt
+import numpy as np
 
 def get_list():
     n = int(input("Enter number of elements in data: "))
     return [int(input(f"Enter element {i+1}: ")) for i in range(n)]
 
 
-def find_quartiles(l):
-    quartiles = []
-    
-
-    return quartiles
-
-
 def main():
-    l = get_list()
+    l = [0, 100, 500, 600, 1000, 100000]
+    #l = sorted(get_list())
     total = sum(l)
     n = len(l)
     mean = total / n
-    stdev = sum([(elt - mean) for elt in l]) / (n-1)
-    var = sqrt(stdev)
+    stdev = round(sum([(elt - mean)**2 for elt in l]) / (n-1), 4)
+    var = round(sqrt(stdev), 4)
+
+    q1, q2, q3 = np.quantile(l, [0.25,0.5,0.75]) 
+
+    IQR = q3 - q1
+    upper_fence = q3 + 1.5 * IQR
+    lower_fence = q1 - 1.5 * IQR
+
+    outliers = [elt for elt in l if (elt > upper_fence or elt < lower_fence)]
 
     print(f'''
     Inputted values: {l}\n
@@ -33,6 +36,11 @@ def main():
     Mean: {mean}\n 
     Standard deviation: {stdev}\n 
     Variance: {var}\n 
+    Q1 - {q1}, Q2 - {q2}, Q3 - {q3}\n
+    IQR: {IQR}\n
+    Upper fence: {upper_fence}\n
+    Lower fence: {lower_fence}\n
+    Outliers: {outliers}
     ''')
 
 
