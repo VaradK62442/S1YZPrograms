9ab09a3e63411af8a089c1827696bf064059a9f2
Varad Kulkarni <83868670+Hades62442@users.noreply.github.com>
2023-01-23 13:33:39 +0000

    independent two means t hypothesis test

--- tHypothesisTests.py
+++ tHypothesisTests.py
@@ -22,9 +22,10 @@ def single_mean():
 
     print(f'''{tailed}-tail hypothesis test with:
     Degrees of Freedom: {df}
+    Significance level: {sig_level/tailed}
     Standard error: {round(se, 4)}
     T score: {-1 * round(t_score, 4)}
-    Rejection region: {-1 * round(rejection_region, 2)}
+    Rejection region: {-1 * round(rejection_region, 4)}
     p value: {round(p_val, 4)}''')
 
     # decision
@@ -65,9 +66,10 @@ def two_means_paired():
     print(f'''{tailed}-tail hypothesis test with:
     Difference in means: {x_bar}
     Degrees of Freedom: {df}
+    Significance level: {sig_level/tailed}
     Standard error: {round(se, 4)}
     T score: {-1 * round(t_score, 4)}
-    Rejection region: {-1 * round(rejection_region, 2)}
+    Rejection region: {-1 * round(rejection_region, 4)}
     p value: {round(p_val, 4)}''')
 
     # decision
@@ -82,7 +84,47 @@ def two_means_paired():
 
 
 def two_means_independent():
-    pass    
+    # do not use unless clearly stated 
+    # use pooled sd only after careful consideration    
+    x_bar1 = float(input("Enter value for x bar 1: "))
+    n1 = int(input("Enter value for n 1: "))
+    sd1 = float(input("Enter value for standard deviation 1: "))
+
+    x_bar2 = float(input("Enter value for x bar 2: "))
+    n2 = int(input("Enter value for n 2: "))
+    sd2 = float(input("Enter value for standard deviation 2: "))
+
+    sig_level = float(input("Enter significance level: "))
+    mu = float(input("Enter hypothesis mean difference: "))
+    tailed = int(input("Enter if the test is [1] tailed or [2] tailed: "))
+
+    df = n1 + n2 - 2
+    s_2_pooled = ((n1-1) * sd1**2 + (n2-1) * sd2**2) / (n1 + n2 - 2)
+    t_score = ((x_bar1 - x_bar2) - mu) / sqrt(s_2_pooled*(1/n1 + 1/n2))   
+
+    if t_score > 0:
+        t_score *= -1
+
+    rejection_region = stats.t.ppf(sig_level/tailed, df) # n tailed in tables
+    p_val = stats.t.cdf(t_score, df) * tailed
+
+    print(f'''{tailed}-tail hypothesis test with:
+    Degrees of Freedom: {df}
+    Significance level: {sig_level/tailed}
+    Standard error pooled: {round(s_2_pooled, 4)}
+    T score: {-1 * round(t_score, 4)}
+    Rejection region: {-1 * round(rejection_region, 4)}
+    p value: {round(p_val, 4)}''')
+
+    # decision
+    if sig_level > p_val or sig_level < -1 * p_val:
+        print(f"Reject null hypothesis.")
+        print(f"T score is inside the rejection region.")
+        print(f"p value is less than significance level.")
+    else:
+        print(f"Fail to reject null hypothesis.")
+        print(f"T score is not inside the rejection region.")
+        print(f"p value is more than significance level.")
 
 
 def main():
@@ -91,11 +133,11 @@ def main():
     \n[3] Difference of two means (independent data)
     \nChoose type: '''))
     
-    if choice == "1":
+    if choice == 1:
         single_mean()
-    elif choice == "2":
+    elif choice == 2:
         two_means_paired()
-    elif choice == "3":
+    elif choice == 3:
         two_means_independent()
 
 
