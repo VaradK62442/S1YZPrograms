137752bd8901950f49d8b1304c58f5a01e3c22e6
Varad Kulkarni <83868670+Hades62442@users.noreply.github.com>
2023-01-23 13:38:38 +0000

    independent two means CI

--- tConfidenceIntervals.py
+++ tConfidenceIntervals.py
@@ -18,9 +18,9 @@ def single_mean():
     print(f'''{tailed}-tailed confidence interval with 
     x bar: {x_bar}
     n: {n}
-    significance level: {100 * (1 - round(sig_level, 4))}%
+    Significance level: {100 * (1 - round(sig_level, 4))}%
     t value: {round(t_val, 4)}
-    standard error: {round(se, 4)}
+    Standard error: {round(se, 4)}
 
     upper bound: {round(upper_bound, 4)}
     lower bound: {round(lower_bound, 4)}
@@ -50,10 +50,10 @@ def two_means_paired():
 
     print(f'''{tailed}-tailed confidence interval with 
     x bar 1 - x bar 2: {x_bar}
-    significance level: {100 * (1 - round(sig_level, 4))}%
+    Significance level: {100 * (1 - round(sig_level, 4))}%
     t value: {-1 * round(t_val, 4)}
-    standard error: {round(se, 4)}
-    df: {df}
+    Standard error: {round(se, 4)}
+    Degrees of freedom: {df}
 
     upper bound: {round(upper_bound, 4)}
     lower bound: {round(lower_bound, 4)}
@@ -61,7 +61,39 @@ def two_means_paired():
 
 
 def two_means_independent():
-    pass
+    # do not use unless clearly stated 
+    # use pooled sd only after careful consideration    
+    x_bar1 = float(input("Enter value for x bar 1: "))
+    n1 = int(input("Enter value for n 1: "))
+    sd1 = float(input("Enter value for standard deviation 1: "))
+
+    x_bar2 = float(input("Enter value for x bar 2: "))
+    n2 = int(input("Enter value for n 2: "))
+    sd2 = float(input("Enter value for standard deviation 2: "))
+
+    sig_level = float(input("Enter significance level: "))
+    tailed = int(input("Enter if the test is [1] tailed or [2] tailed: "))
+
+    df = n1 + n2 - 2
+    t_val = stats.t.ppf(sig_level/tailed, df)
+    s_2_pooled = ((n1-1) * sd1**2 + (n2-1) * sd2**2) / (n1 + n2 - 2)
+    se = sqrt(s_2_pooled * (1/n1 + 1/n2))
+
+    x_bar = x_bar1 - x_bar2
+
+    upper_bound = x_bar + t_val * se
+    lower_bound = x_bar - t_val * se
+
+    print(f'''{tailed}-tailed confidence interval with 
+    x bar 1 - x bar 2: {x_bar}
+    Significance level: {100 * (1 - round(sig_level, 4))}%
+    t value: {-1 * round(t_val, 4)}
+    Standard error: {round(se, 4)}
+    Degrees of freedom: {df}
+
+    upper bound: {round(upper_bound, 4)}
+    lower bound: {round(lower_bound, 4)}
+    ''')
 
 
 def main():
