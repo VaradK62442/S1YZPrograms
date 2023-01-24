fcbab90e197e4a6f7b08020a0a9392ab1ff18e06
Varad Kulkarni <83868670+Hades62442@users.noreply.github.com>
2023-01-24 13:52:09 +0000

    function to find power level

--- powerCalculations.py
+++ powerCalculations.py
@@ -3,7 +3,27 @@ from math import sqrt
 from math import ceil
 
 
-def power_cal():
+def power_level():
+    sig_level = float(input("Enter significance level: "))
+    se = float(input("Enter standard error: "))
+    df = int(input("Enter degrees of freedom: "))
+    tailed = int(input("Enter if test is [1] or [2] tailed: "))
+    effect_interest = float(input("Enter the size effect of interest: "))
+
+    t_score = stats.t.ppf(sig_level/tailed, df)
+    effect_size = t_score * se
+    z_score = (effect_size - effect_interest) / (se**2)
+    power = round(stats.norm.cdf(z_score), 4)
+
+    print(f'''
+    T score: {round(t_score, 4)}
+    Effect size should be < {round(effect_size, 4)}
+    P(Y < {round(effect_size, 4)}) = P(Z < {round(z_score, 4)}) = {power}
+    The power of this test is around {power * 100}%
+    ''')
+
+
+def calc_n():
     sig_result = float(input("Enter practically significant result: "))
     sig_level = float(input("Enter significance level: "))
     sd = float(input("Enter standard deviation: "))
@@ -27,7 +47,14 @@ def power_cal():
 
 
 def main():
-    power_cal()
+    choice = int(input('''\n[1] Find power level
+    \n[2] Find n
+    \nChoose type: '''))
+    
+    if choice == 1:
+        power_level()
+    elif choice == 2:
+        calc_n()
 
 
 if __name__ == "__main__":
