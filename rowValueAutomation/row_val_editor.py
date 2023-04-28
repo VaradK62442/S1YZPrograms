f5adcfaa8fe3e76d68c949b3f3d900b444bce588
Varad Kulkarni <83868670+Hades62442@users.noreply.github.com>
2023-04-28 15:14:39 +0100

    improve row value automation scirpt

--- rowValueAutomation/row_val_editor.py
+++ rowValueAutomation/row_val_editor.py
@@ -1,13 +1,15 @@
 def change_row_vals(fname, val):
     # increment value of rows by given val
-    with open(fname) as f, open("output.txt", "w") as w:
+    with open(fname) as f, open("./rowValueAutomation/output.txt", "w") as w:
         for line in f.readlines():
-            print(line)
             if "row=" in line:
-                cur_val = int(line[line.index("row=") + 4])
+                row_index = line.index("row=")
+                equals_index = row_index + 3
+                cur_val_index = line[row_index:].index(",") + row_index
+                cur_val = int(line[equals_index+1:cur_val_index])
                 new_val = cur_val + val
                 line = line.replace("row="+str(cur_val), "row="+str(new_val))
             w.writelines(line)
 
 
-change_row_vals("rows.txt", -1)
+change_row_vals("./rowValueAutomation/rows.txt", -1)
